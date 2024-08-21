60d9fd1fd78918f90bab14355c654cc7b36d14e0
Ali123cool <amezzedd@gmail.com>
2024-08-21 12:35:38 -0500

    s

--- Html-For-Webflow/StartHereForm.js
+++ Html-For-Webflow/StartHereForm.js
@@ -10,7 +10,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 document.getElementById('fetchDataBtn').addEventListener('click', async function() {
     try {
         const { data, error } = await supabase
-            .from('rooferscout_material_costs')
+            .from('rooferscout_material_cost')
             .select('*');
 
         if (error) {
