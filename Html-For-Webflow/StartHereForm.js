23c576249452eef1f42a462ea9c524e17bf1a659
Ali123cool <amezzedd@gmail.com>
2024-08-21 00:08:28 -0500

    testing if it automatically changes the page without me having to republsih

--- Html-For-Webflow/StartHereForm.js
+++ Html-For-Webflow/StartHereForm.js
@@ -10,7 +10,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 document.getElementById('fetchDataBtn').addEventListener('click', async function() {
     try {
         const { data, error } = await supabase
-            .from('rooferscout_building_type_factors')
+            .from('rooferscout_material_costs')
             .select('*');
 
         if (error) {
