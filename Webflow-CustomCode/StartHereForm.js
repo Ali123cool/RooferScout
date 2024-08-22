e772cc827af629cc01034c39b86be6488d5f491e
Ali123cool <amezzedd@gmail.com>
2024-08-21 19:43:42 -0500

    sddfs

--- Webflow-CustomCode/StartHereForm.js
+++ Webflow-CustomCode/StartHereForm.js
@@ -134,7 +134,7 @@ async function fetchSupabaseData(tableName) {
     try {
         const { data, error } = await supabase
             .from(tableName) // This ensures the API key is included in the request headers
-            .select('*'); // Adjust this line based on what you need to select
+            .select(); // Adjust this line based on what you need to select
 
         if (error) {
             console.error(`Error fetching data from ${tableName}:`, error.message, error.details);
